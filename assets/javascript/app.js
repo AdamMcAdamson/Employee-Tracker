f7daa1b448caa4269555fc94513bc213b8f531be
Adam Brunn <adambrunn1@hotmail.com>
2017-03-29 12:53:47 -0500

    Commiting for pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,29 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyDsnA4ikH4MWEr81ERGiNdph28E7Tihdqw",
     authDomain: "employee-tracker-722e2.firebaseapp.com",
     databaseURL: "https://employee-tracker-722e2.firebaseio.com",
     storageBucket: "employee-tracker-722e2.appspot.com",
     messagingSenderId: "987085544007"
-  };
-  firebase.initializeApp(config);
+};
+
+firebase.initializeApp(config);
 
 
 // Variable to reference the database
-    var database = firebase.database();
+var database = firebase.database();
 
-    // Employee details as variables to reference the database
-   var name = "";
-    var role = "";
-    var start = "";
-    var rate = "";
+// Employee details as variables to reference the database
+var name = "";
+var role = "";
+var start = "";
+var rate = "";
 
 
-    // Capture Button Click
-    $("#add-user").on("click", function() {
-      // Don't refresh the page!
-      event.preventDefault();
+// Capture Button Click
+$("#add-user").on("click", function() {
+  // Don't refresh the page!
+  event.preventDefault();
 
   // Input form
-
-  
-
+}
