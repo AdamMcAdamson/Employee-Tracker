54922598951b24d89e5a6442ab683a7d0f185862
Adam Brunn <adambrunn1@hotmail.com>
2017-03-29 13:23:40 -0500

    syncing progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,15 @@ var start = "";
 // All of our employees will be stored in this directory.
 var connectionsRef = database.ref("/employees");
 
+connectionsRef.on("child_added", function(snapshot){
+  var elemName = snapshot.val().name;
+  var elemRole = snapshot.val().role;
+  var elemRate = snapshot.val().rate;
+  var elemStart = snapshot.val().start;
+  var elemDateAdded = snapshot.val().dateAdded;
+  console.log();
+});
+
 
 //This takes the form input and adds to the database
 // Capture Button Click
@@ -33,12 +42,13 @@ $("#?add-user?").on("click", function() {
     role = $("#role-input").val().trim();
     rate = $("#rate-input").val().trim();
     start = $("#start-input").val().trim();
-
+    dateAdded = "Time";
     // Change what is saved in firebase
     connectionsRef.push({
         name: name,
         role: role,
         rate: rate,
         start: start,
+        dateAdded: dateAdded
     });
 });
